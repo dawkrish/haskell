5aa646d337de11c394fe8402d6f9faa50a1e1289
krishnanshagarwal112 <gonkillua2680@gmail.com>
2024-08-05 19:53:43 +0530

    nim working

--- hutton/nim_game.hs
+++ hutton/nim_game.hs
@@ -1,11 +1,23 @@
 main :: IO()
 main = do
-  putStr "Player 1, how many stars to remove: "
-  x <- getChar
-  putStrLn ""
-  putStr "Player 2, how many stars to remove: "
-  getChar
-  y <- getChar
-  return ()
+  play [5,4,3,2,1]
   --getLine
   --putStrLn [x]
+
+play :: [Int] -> IO()
+play board = do
+  putStr (visualize board)
+  putStr "select row number: "
+  r <- getChar
+  putStrLn ""
+  putStr "select no. of stars: "
+  n <- getChar
+  putStrLn ""
+
+
+visualize :: [Int] -> String
+visualize xs = concat [show (i+1) ++ ": " ++ foo (xs !! i)  ++ "\n" | i <- [0..length xs - 1]]
+  where foo n = concat (replicate n "* ")
+
+update :: [Int] -> Int -> Int -> [Int]
+update xs r n = [if (r - 1) == i then (xs!!i) - n else xs !! i | i <- [0..length xs - 1] ]
